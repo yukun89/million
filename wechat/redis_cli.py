395568e07301867e43b9b7cdcb38b99236429be4
yukun89 <hykhyy@qq.com>
2021-06-13 19:48:31 +0800

    remove unused file

--- wechat/redis_cli.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import redis    # 导入redis 模块
-
-pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
-g_redis = redis.Redis(host='localhost', port=6379, decode_responses=True) 
-
-
