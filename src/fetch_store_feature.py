3e483f395d94c57b6ecdceb76e91de9d7d4b202a
huangyukun <huangyukun@didiglobal.com>
2024-02-18 22:46:16 +0800

    update coin market

--- src/fetch_store_feature.py
+++ src/fetch_store_feature.py
@@ -35,6 +35,34 @@ def update_greedy_fear_index(is_batch=False):
     return
 
 
+def update_coin_info():
+    coin_list = etc.get_coin_list()
+    id_symbol_dict = {}
+    for coin in coin_list:
+        symbol_id = coin["id"]
+        symbol = coin["symbol"]
+        id_symbol_dict[symbol_id] = symbol
+        coin_line = orm.Schema.CoinList(id=symbol_id, name=coin["name"], symbol=symbol)
+        orm.session.merge(coin_line)
+    orm.session.commit()
+
+    max_supply_updated_hour_ts = int(time.time()/3600)*3600
+    for symbol_id, symbol in id_symbol_dict.items():
+        this_market_data_list = etc.get_market_data(symbol_id)
+        this_market_data = this_market_data_list[0]
+        total_supply = int(this_market_data["total_supply"])
+        max_supply = int(this_market_data["max_supply"])
+        coin_line = orm.Schema.CoinMarket(id=symbol_id,
+                                          symbol=symbol,
+                                          total_supply=total_supply,
+                                          max_supply=max_supply,
+                                          max_supply_updated_hour_ts=max_supply_updated_hour_ts)
+        orm.session.add(coin_line)
+    orm.session.commit()
+    return
+
+
+
 def update_kline_data_all(instId, bar, from_ts=0, to_ts=0):
     # 获取从2018年以后的所有数据
     if bar not in common.bar_list:
@@ -55,9 +83,9 @@ def update_kline_data_all(instId, bar, from_ts=0, to_ts=0):
         after = cur
         resp = public_wrapper.marketDataAPI.get_history_candlesticks(instId=instId,
                                                                      bar=bar,
-                                                                     before=before*1000,
-                                                                     after=after*1000,
-                                                                     limit=batch_num+1)
+                                                                     before=before * 1000,
+                                                                     after=after * 1000,
+                                                                     limit=batch_num + 1)
         if resp['code'] != '0':
             print("Error: failed to get k line. instId = %s || bar = %s || before = %s || from = %s" % (
                 instId, bar, before, timestamp2datetime(before)))
@@ -71,7 +99,7 @@ def update_kline_data_all(instId, bar, from_ts=0, to_ts=0):
         cur -= step
         for line in data:
             ts = int(int(line[0]) / 1000)
-            mtime=timestamp2datetime(ts)
+            mtime = timestamp2datetime(ts)
             o = float(line[1])
             h = float(line[2])
             l = float(line[3])
@@ -103,4 +131,4 @@ def update_kline_data_all(instId, bar, from_ts=0, to_ts=0):
 
 if __name__ == "__main__":
     # update_greedy_fear_index(is_batch=False)
-    update_kline_data_all("BTC-USDT", "1H", common.start_ts, common.start_ts + 86400*366)
+    update_kline_data_all("BTC-USDT", "1H", common.start_ts, common.start_ts + 86400 * 366)
